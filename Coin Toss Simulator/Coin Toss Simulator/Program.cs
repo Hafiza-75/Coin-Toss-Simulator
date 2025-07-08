971969543b5a935a1ecb84032f6834d7a3c1ad22
Hafiza-75 <123856356+Hafiza-75@users.noreply.github.com>
2025-07-08 12:11:22 +0500

    Add files via upload

---
+++ Coin Toss Simulator/Coin Toss Simulator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin_Toss_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#   ...............Coin Toss Simulator............    #");
+            Random rand = new Random();
+
+            while (true)
+            {
+                Console.Write("\n Type toss to flip the coin and quit to exit: ");
+                string input = Console.ReadLine().ToLower().Trim();
+
+                if (input == "exit")
+                {
+                    Console.WriteLine("\nThanks  for playing! \n |GoodBye|");
+                    break;
+                }
+                if (input != "toss")
+                {
+                    Console.WriteLine("\nInvalid input!");
+                    continue;
+                }
+
+                string result = rand.Next(2) == 0 ? "Heads" : "Tails";
+                Console.WriteLine($"Result: {result}");
+
+            }
+        }
+    }
+}
